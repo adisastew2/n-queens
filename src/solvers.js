c2d36bd9cdb7e6955e41ccfc9364010fc32a49de
Hack Reactor Student <student@hackreactor.com>
2017-07-13 21:19:54 -0700

    adds queen solution code

--- src/solvers.js
+++ src/solvers.js
@@ -18,48 +18,68 @@
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
   //make an empty matrix
-  var makeNewMatrix = function(n) {
+  /*var makeNewMatrix = function(n) {
     return _(_.range(n)).map(function() {
       return _(_.range(n)).map(function() {
         return 0;
       });
     });
   };
-  //debugger;
+  debugger;
   var solutionMatrix = makeNewMatrix(n);
 
   //check rooks placed
   var rooksPlaced = 0;
-  debugger;
   if (n === 1) {
     solutionMatrix = [[1]];
     rooksPlaced++;
   } else {
-    //iterate through rows
-    for (var i = 0; i < solutionMatrix.length; i++) {
-      // row i doesn't have a rook
-      var rooksInRow = solutionMatrix[i].reduce( (rooks, square) => rooks + square);
-      if (rooksInRow === 0) {
-        //iterate columns and columns
-        for (var j = 0; j < solutionMatrix[i]; j++) {
-          //if rooks placed is less than n
-          if (rooksPlaced < n) {
-            //place rook in first position matrix[0][0]
-            solutionMatrix[i][j] = 1;
-            rooksPlaced++;
-          }
+    //debugger;
+    var columnsUsed = [];
+    for (var i = 0; i < solutionMatrix.length; i++) {    
+    
+      for (var j = 0; j < solutionMatrix[i].length; j++) {
+        var rooksInRow = solutionMatrix[i].reduce( (rooks, square) => rooks + square);
+        if (rooksInRow === 0 && rooksPlaced < n) {
+          if (!columnsUsed.includes(j)) {
+            solutionMatrix[i].splice(j, 1, 1);
+            columnsUsed.push(j);
+            rooksPlaced++;         
+          }         
         }
+      }
+    }
+  }*/
+  var solutionCount = 0;
+
+  var board = new Board({ n: n});
+
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      board.togglePiece(i, j);
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(i, j);
       }
     }
   }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutionMatrix));
-  return solutionMatrix;
+  solution = board.rows();
+  
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+
+  var factorial = function (num) {
+    if (num === 0) {
+      return 1;
+    }
+    return num * factorial(num - 1);
+  };
+  var solutionCount = factorial(n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -68,6 +88,27 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
+
+  var board = new Board({ n: n});
+  //queens placed equals zero
+  var queensPlaced = 0;
+
+  for (var i = 0; i < n; i++) {
+    
+    for (var j = 0; j < n; j++) {
+      //try place a piece for every possible index => recursive(0, j)
+      board.togglePiece(i, j);
+      if (board.hasAnyQueensConflicts()) {
+        board.togglePiece(i, j);
+        //increment queens places
+        queensPlaced++;
+        //if queens placed equals n, increase solution count, and solution equals board
+        if (queensPlaced = n) {
+          solution = board.rows();
+        }
+      }
+    }
+  }
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
